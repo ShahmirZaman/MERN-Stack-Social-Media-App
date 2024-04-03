2292da66fee5a2fe2e2c51be0d9e755b855cc2fb
Shahmir Zaman <shahmirzaman009@gmail.com>
2024-04-04 04:18:12 +0500

    react router added

--- Frontend Client/src/App.js
+++ Frontend Client/src/App.js
@@ -1,10 +1,23 @@
 import Home from './pages/Home/Home'
+import Profile from './pages/Profile/Profile'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
